494e202ccaeabca0e58ab02a53d44b0656a55700
Max Ruffo <Max.Ruffo@Student.HTW-Berlin.de>
2023-06-16 14:02:54 +0200

    create_databse_structure.py erstelle erstellt die datenbank

---
+++ scr/modules/database/create_database_structure.py
@@ -0,0 +1,68 @@
+import sqlite3
+import os
+
+#from insert_assets.insert_crypto_into_assets import _insert_crypto
+#from insert_assets.insert_stocks_into_assets import _insert_stocks
+#from insert_pricedata.insert_data_into_pricedata import _insert_pricedata
+
+
+def init_database():
+    database_folder = 'resources/database'
+    database_file = 'all_crypto_database.db'
+    database_path = os.path.join(database_folder, database_file)
+
+    # Überprüfen, ob der Ordner bereits existiert, andernfalls erstellen
+    if not os.path.exists(database_folder):
+        os.makedirs(database_folder)
+
+    # Überprüfen, ob die Datenbankdatei bereits existiert
+    if not os.path.exists(database_path):
+        # Verbindung zur Datenbank herstellen oder eine neue Datenbank erstellen
+        conn = sqlite3.connect(database_path)
+
+        # Ein Cursor-Objekt erstellen, um SQL-Abfragen auszuführen
+        cursor = conn.cursor()
+
+        # Erste Tabelle erstellen
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS Assets (
+                ticker TEXT PRIMARY KEY,
+                name TEXT,
+                sector TEXT,
+                type TEXT
+            )
+        ''')
+
+        # Zweite Tabelle erstellen
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS PriceData (
+                ticker TEXT NOT NULL,
+                timestamp DATE NOT NULL,
+                timestamp_offset TEXT NOT NULL,
+                open REAL,
+                high REAL,
+                low REAL,
+                close REAL,
+                adj_close REAL,
+                volume REAL,
+                PRIMARY KEY (ticker, timestamp),
+                FOREIGN KEY (ticker) REFERENCES Assets (ticker)
+            )
+        ''')
+
+
+
+        # Änderungen speichern und die Verbindung zur Datenbank schließen
+        conn.commit()
+        conn.close()
+        print(f'Datenbankdatei {database_file} wurde erstellt.')
+    else:
+        print(f'Datenbankdatei {database_file} existiert bereits.')
+
+
+'''
+Für die erstellung der Big Database wo sich alle Ticker und Price Daten befinden
+'''
+
+init_database()
+
--- scr/modules/scraper/converter.py
+++ scr/modules/scraper/converter.py
@@ -1,5 +1,6 @@
 import csv
 import json
+import os
 
 def convert_csv_to_json(csv_file, json_file):
     # Daten aus der CSV-Datei lesen
@@ -11,7 +12,31 @@ def convert_csv_to_json(csv_file, json_file):
     with open(json_file, 'w') as file:
         json.dump(data, file, indent=4)
 
-# Beispielaufruf
+"""# Beispielaufruf
 csv_file = 'resources/index-list/~index_cryptocurrencies_list.csv'
 json_file = 'resources/index-list/~index_cryptocurrencies_list.json'
 convert_csv_to_json(csv_file, json_file)
+"""
+
+
+
+def set_ticker_list_config_json(tickers):
+    output_folder = "config"
+    output_file = os.path.join(output_folder, "ticker_list.json")
+
+    # Überprüfen, ob der Ausgabeordner existiert, andernfalls erstellen
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
+
+    ticker_list = {"tickers": tickers}
+
+    with open(output_file, 'w') as file:
+        json.dump(ticker_list, file)
+
+    print(f"Die Ticker-Liste wurde in '{output_file}' gespeichert.")
+
+
+tickers = ["BTC", "ETH", "XRP", "LTC", "ADA"]
+output_file = "ticker_list.json"
+
+set_ticker_list_config_json(tickers)
